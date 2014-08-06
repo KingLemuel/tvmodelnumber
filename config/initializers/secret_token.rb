053fe4f4a233022ca85ba2824066544f2aaddbd0
Lemuel <lemueluhuru@gmail.com>
2014-08-06 16:16:12 -0400

    initital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tvmodelnumber::Application.config.secret_key_base = 'f243768e76ac59ae26452a89e3c41ea72ae35dfdf3a27bf1b36b7242221904baabc4639d48976b14f67e009e8b6218066dff9bbc967b027337ff40c7ff19996b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Tvmodelnumber::Application.config.session_store :cookie_store, key: '_tvmodelnumber_session'
